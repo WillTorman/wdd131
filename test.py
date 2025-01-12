79477c9b222aed057168c21e4b701aa46f7a8fa2
Will Torman <willtorman@gmail.com>
2025-01-11 17:52:58 -0700

    done

---
+++ test.py
@@ -0,0 +1,6 @@
+# f string for easy sintax
+color = input("What is your favorite color? ")
+print(f"Your favorite color is: {color}")
+
+# beginner friendly
+print("Your favorite color is: " + color)
