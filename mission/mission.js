c111673bb632c14bdcfe8adf67d06a48c82e0b02
Will Torman <willtorman@gmail.com>
2025-01-18 20:38:29 -0700

    theme changes

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const selectElem = document.querySelector("select");
+const content = document.querySelector(".content");
+const logo = document.querySelector("img");
+
+selectElem.addEventListener("change", changeTheme);
+
+function changeTheme() {
+    const theme = selectElem.value;
+    if (theme === "dark") {
+        content.classList.add("dark");
+        logo.src = "byui-logo_white.png"
+    } else {
+        content.classList.remove("dark");
+    }
+}
