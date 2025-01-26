4ce9fe7e4f92f60b23e34919c426d6aee37a09fe
Will Torman <willtorman@gmail.com>
2025-01-25 23:06:05 -0700

    cool pics

--- test.py
+++
@@ -1,6 +0,0 @@
-# f string for easy sintax
-color = input("What is your favorite color? ")
-print(f"Your favorite color is: {color}")
-
-# beginner friendly
-print("Your favorite color is: " + color)
