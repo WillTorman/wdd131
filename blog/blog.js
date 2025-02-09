d82beaaa18eb9df626b7d8c670536e4fa64a208b
Will Torman <willtorman@gmail.com>
2025-02-08 19:46:34 -0700

    progress

---
+++ blog/blog.js
@@ -0,0 +1,27 @@
+const articles = [
+    {
+        id: 1,
+        title: 'I Am Number Four',
+        date: 'August 3, 2010',
+        description:
+            'If you enjoy Sci-Fi action books this is a must read.',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/en/6/6f/I_Am_Number_Four_Cover.jpg',
+        imgAlt: 'Book cover for I Am Number Four',
+        ages: '12-18',
+        genre: 'Young adult, Science fiction',
+        stars: '⭐⭐⭐⭐⭐'
+    },
+    {
+        id: 2,
+        title: 'Harry Potter and the Goblet of Fire',
+        date: 'July 8, 2000',
+        description:
+            'A classic and must read! If you enjoy adventure, plot twists, and fantasy, this book is for you.',
+        imgSrc:
+            'https://upload.wikimedia.org/wikipedia/en/b/b6/Harry_Potter_and_the_Goblet_of_Fire_cover.png',
+        imgAlt: 'Book cover for Harry Potter and the Goblet of Fire',
+        ages: '12-17',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐⭐'
+    }
+]
--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -36,13 +36,24 @@ function viewerTemplate(pic, alt) {
 
 function viewHandler(event) {
     // create a variable to hold the element that was clicked on from event.target
+    const img = event.target.closest("img");
 
     // get the src attribute from that element and 'split' it on the "-"
+    let attribute = img.getAttribute('src');
+    let beg = attribute.split("-");
 
     // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    let full = beg[0] + "-full.jpeg";
+    console.log(full);
 
     // insert the viewerTemplate into the top of the body element
     // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    let body = document.querySelector("body");
+    let html = viewerTemplate(full, "random image");
+
+    body.insertAdjacentHTML("afterbegin", html);
+
+    let modalx = document.querySelector(".close-viewer");
 
     // add a listener to the close button (X) that calls a function called closeViewer when clicked
 
